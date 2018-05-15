8b3fe45b9a707bcea93ebfc74941925f8e16e445
DESKTOP-QG2DL2H\chira <shahc@oregonstate.edu>
2018-05-14 17:26:30 -0700

    Initial commit - Solution works

--- imageDefectAnalysis.cpp
+++ imageDefectAnalysis.cpp
@@ -1,8 +1,6 @@
 // Solar cell defect analysis
 
 #include<opencv2/opencv.hpp>
-//#include <opencv2/highgui/highgui.hpp>
-//#include "opencv2/imgproc/imgproc.hpp"
 #include<iostream>
 #include<conio.h>          
 using namespace cv;
